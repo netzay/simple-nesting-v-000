c3cbbb3983386aad6de7f5f9eeb4e151c501c5da
Ally Netzel <netzelally@gmail.com>
2018-04-04 22:09:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -2,6 +2,7 @@
 def hopper
 	programmer_hash = 
  		{
+ 	
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -15,8 +16,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,17 +57,19 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -100,8 +104,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+      alans_new_info = "GUI"
+      programmer_hash[:alan_kay][:known_for] ="GUI"
+
      
 end
 
